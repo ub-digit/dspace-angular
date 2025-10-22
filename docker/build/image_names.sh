bce761a2d9f1b8702f337a7394426527db4503f1
David Gustafsson <david.gustafsson@ub.gu.se>
2025-10-22 15:21:51 +0200

    Fix typos

--- docker/build/image_names.sh
+++ docker/build/image_names.sh
@@ -5,6 +5,6 @@ else
   GUB_ANGULAR_VERSION=$1
 fi
 
-PRODUCION_IMAGE=docker.ub.gu.se/dspace-angular:dspace-8_x-${GUB_ANGULAR_VERSION}-dist
+PRODUCTION_IMAGE=docker.ub.gu.se/dspace-angular:dspace-8_x-${GUB_ANGULAR_VERSION}-dist
 DEVELOPMENT_IMAGE=docker.ub.gu.se/dspace-angular:dspace-8_x-dev # No versioned image to save spacek, needs docker compose pull
 
