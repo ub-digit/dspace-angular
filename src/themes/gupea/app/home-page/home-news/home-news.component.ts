81abc1fd394629963278bd4237d826d6f18f1b90
David Gustafsson <david.gustafsson@ub.gu.se>
2025-11-17 20:29:33 +0100

    Translate strings in home news compoent

--- src/themes/gupea/app/home-page/home-news/home-news.component.ts
+++ src/themes/gupea/app/home-page/home-news/home-news.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
 import { HomeNewsComponent as BaseComponent } from '../../../../../app/home-page/home-news/home-news.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'ds-themed-home-news',
   styleUrls: ['./home-news.component.scss'],
   templateUrl: './home-news.component.html',
+  imports: [TranslateModule],
   standalone: true,
 })
 
